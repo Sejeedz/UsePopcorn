31b4116655c5b5873cf579cac20d789121d749b9
Sejeedz <m.sajid3421@gmail.com>
2024-11-07 02:35:51 +0500

    Created

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was set to {movieRating} stars</p>
+//     </div>
+//   );
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <StarRating
+      maxRating={"5"}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} className="test" color="purple" defaultRating={3} />
+
+    <Test /> */}
+    <App />
+  </React.StrictMode>
+);
